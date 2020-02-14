31cfc1e0132f6ba96c4ca38d6b5ce631f25287c6
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-14 19:03:39 +0530

    [Mayank] Refactor. Adding gameplay check option functionality

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,2 +1,44 @@
 #!/bin/bash
+player1=0
+playerCurrentPosition()
+{
+    value=$player1
+#echo "player1's intial position is:$player1 th position"
+    return $value
+}
 
+diceValue()
+{
+diceRoll=$(((RANDOM%6)+1))
+#echo "number appeared on Dice is :$diceRoll"
+    return $diceRoll
+}
+
+checkOption()
+{
+result=$((RANDOM%3))
+if((result==0))
+then
+echo "No play"
+echo "player1 is at position:$player1"
+
+elif((result==1))
+then
+echo "Ladder"
+diceValue
+diceNumber=$?
+playerCurrentPosition
+position=$?
+player1=$((position+diceNumber))
+echo "player1 is at position:$player1"
+else
+echo "snake"
+diceValue
+diceNumber=$?
+playerCurrentPosition
+position=$?
+player1=$((position-diceNumber))
+echo "player1 is at position:$player1"
+fi
+}
+checkOption
