2a26e5dcac50ca09b4449da9c326ac68319bdab4
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-14 16:04:49 +0530

    [Mayank] Refactor. game board creation and display

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
+echo "x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x"
+echo "x                                                                     x"
+echo "x        WELCOME TO SNAKE AND LADDER BOARD GAME                       x"
+echo "x                                                                     x"
+echo "x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x"
 
-echo "x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x==x=x=x=x"
-echo "        WELCOME TO SNAKE AND LADDER BOARD GAME              "
-echo "x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x==x=x=x=x"
+counter=0
+
+for (( i = 10; i>0; i-- ))
+do
+echo "+------+------+------+------+------+------+------+------+------+------+"
+echo "|      |      |      |      |      |      |      |      |      |      |"
+echo -n "|"
+
+for (( j = 1; j < 11; j++ ))
+do
+
+if((counter>90))
+then
+	echo -n "   $(((i*10+1)-j))  |"
+# echo -n " "
+else	
+if((counter==0))
+then
+	# echo -n " "
+	echo -n " $(((i*10+1)-j))  |"
+else																										
+echo -n "  $(((i*10+1)-j))  |"
+fi
+fi
+counter=$((counter+1))
+done
+echo ""
+echo "|      |      |      |      |      |      |      |      |      |      |"
+echo "+------+------+------+------+------+------+------+------+------+------+"
+done
+
+
