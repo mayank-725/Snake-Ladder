06ea553d9a43e7b41cc5a6ee303979f8265c29fb
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-15 12:32:08 +0530

    [Mayank] Refactor. changes to winning condition of player 1 logic done

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,2 +1,65 @@
 #!/bin/bash
+player1=0
+playerCurrentPosition()
+{
+    value=$player1
+#echo "player1's intial position is:$player1 th position"
+    return $value
+}
 
+diceValue()
+{
+diceRoll=$(((RANDOM%6)+1))
+#echo "number appeared on Dice is :$diceRoll"
+    return $diceRoll
+}
+
+checkOption()
+{
+while((player1<100))
+do
+result=$((RANDOM%3))
+if((result==0))
+then
+echo "No play"
+echo "player1 is at position:$player1"
+
+elif((result==1))
+then
+echo "Ladder"
+diceValue
+diceNumber=$?
+echo "Dice Roll:$diceNumber"
+playerCurrentPosition
+position=$?
+player1=$((position+diceNumber))
+echo "player1 is at position:$player1"
+else
+echo "snake"
+diceValue
+diceNumber=$?
+playerCurrentPosition
+position=$?
+player1=$((position-diceNumber))
+  if((player1<0))
+  then
+	player1=0
+	echo "player1 is at position:$player1"
+  else
+	echo "player1 is at position:$player1"
+  fi
+fi
+echo ""
+
+
+if((player1>100))
+then
+	player1=$((player1-diceNumber))
+elif((player1==100))
+then
+echo "Player1 won the game"
+fi
+done
+
+}
+checkOption
